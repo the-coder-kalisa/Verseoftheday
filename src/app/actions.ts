e6cdc4617929e033fd665b2ac11fff96031edafa
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-01-16 23:20:38 +0200

    Update cron schedule for posting verse

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -40,7 +40,7 @@ export const postVerse = async () => {
   return response;
 };
 
-cron.schedule("21 23 * * *", async () => {
+cron.schedule("25 23 * * *", async () => {
   verse = await fetchVerse();
   await postVerse();
 });
