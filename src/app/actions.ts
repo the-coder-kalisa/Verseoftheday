8b85649d302229b05208dbedca6a85e0d8c0afb9
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-01-16 23:21:58 +0200

    update time

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -40,7 +40,7 @@ export const postVerse = async () => {
   return response;
 };
 
-cron.schedule("2 23 * * *", async () => {
+cron.schedule("25 23 * * *", async () => {
   verse = await fetchVerse();
   await postVerse();
 });
